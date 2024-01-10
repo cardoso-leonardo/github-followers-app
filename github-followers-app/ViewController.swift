233ee3ecf2772c57b01810a83362e44bbe591d48
Leonardo Cardoso <cardoso.leonardo@outlook.com>
2024-01-10 20:02:51 -0300

    chore: initial setup to use viewcode

---
+++ github-followers-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  github-followers-app
+//
+//  Created by Leonardo Cardoso on 10/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
