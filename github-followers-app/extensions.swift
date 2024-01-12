aa7e9cbc86913cbd56460b654f9bfb144e2951ac
Leonardo Cardoso <cardoso.leonardo@outlook.com>
2024-01-12 17:13:14 -0300

    feat: added extension to support multiple views in addSubview method

---
+++ github-followers-app/extensions.swift
@@ -0,0 +1,16 @@
+//
+//  extensions.swift
+//  github-followers-app
+//
+//  Created by Leonardo Cardoso on 12/01/24.
+//
+
+import UIKit
+
+extension UIView {
+    func addSubviews(views: UIView...) {
+        for view in views {
+            addSubview(view)
+        }
+    }
+}
