3b597258f5babc242758800990de07edd4d568d0
Leonardo Cardoso <cardoso.leonardo@outlook.com>
2024-01-13 18:20:41 -0300

    fix: changed returnKeyType

--- github-followers-app/GFTextField.swift
+++ github-followers-app/GFTextField.swift
@@ -36,6 +36,8 @@ class GFTextField: UITextField {
         
         backgroundColor = .tertiarySystemBackground
         autocorrectionType = .no
+        keyboardType = .default
+        returnKeyType = .go
         placeholder = "Enter a username"
     }
     
