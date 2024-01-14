31862d5da9c5ba993f0f75769003b8c0ea975ec6
Leonardo Cardoso <cardoso.leonardo@outlook.com>
2024-01-14 20:26:24 -0300

    feat: created 2 custom UILabel to use as title and body

---
+++ github-followers-app/GFBodyLabel.swift
@@ -0,0 +1,39 @@
+//
+//  GFBodyLabel.swift
+//  github-followers-app
+//
+//  Created by Leonardo Cardoso on 14/01/24.
+//
+
+import UIKit
+
+class GFBodyLabel: UILabel {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    init(textAlignment: NSTextAlignment) {
+        super.init(frame: .zero)
+        self.textAlignment = textAlignment
+        configure()
+    }
+    
+    
+    private func configure() {
+        lineBreakMode = .byWordWrapping
+        textColor = .secondaryLabel
+        font = UIFont.preferredFont(forTextStyle: .body)
+        adjustsFontSizeToFitWidth = true
+        minimumScaleFactor = 0.75
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+}
---
+++ github-followers-app/GFTitleLabel.swift
@@ -0,0 +1,39 @@
+//
+//  GFTitleLabel.swift
+//  github-followers-app
+//
+//  Created by Leonardo Cardoso on 14/01/24.
+//
+
+import UIKit
+
+class GFTitleLabel: UILabel {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    init(textAlignment: NSTextAlignment, fontSize: CGFloat) {
+        super.init(frame: .zero)
+        self.textAlignment = textAlignment
+        self.font = UIFont.systemFont(ofSize: fontSize, weight: .bold)
+        configure()
+    }
+    
+    
+    private func configure() {
+        lineBreakMode = .byTruncatingTail
+        textColor = .label
+        adjustsFontSizeToFitWidth = true
+        minimumScaleFactor = 0.9
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+}
