60b55cf6d6e725a77c4ccded5aa74975ef7607ae
Leonardo Cardoso <cardoso.leonardo@outlook.com>
2024-01-14 21:07:35 -0300

    fix: extensions names and new func

---
+++ github-followers-app/UIVIewController+Ext.swift
@@ -0,0 +1,24 @@
+//
+//  UIVIewController+Ext.swift
+//  github-followers-app
+//
+//  Created by Leonardo Cardoso on 14/01/24.
+//
+
+import UIKit
+
+extension UIViewController {
+    
+    func presentAlertOnMainThread(title: String, message: String, buttonTitle: String) {
+        
+        let alertVC = AlertVC(title: title, message: message, buttonTitle: buttonTitle)
+        alertVC.modalTransitionStyle = .crossDissolve
+        alertVC.modalPresentationStyle = .overFullScreen
+        
+        DispatchQueue.main.async {
+            self.present(alertVC, animated: true)
+        }
+        
+    }
+    
+}
