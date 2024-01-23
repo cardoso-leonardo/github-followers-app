645d135991cfd77eb3a2f7c05d565ca71623a9f3
Leonardo Cardoso <cardoso.leonardo@outlook.com>
2024-01-22 21:07:52 -0300

    fix: function to create three column flow layout was not being called to configure collectionView

--- github-followers-app/Screens/FollowerListVC.swift
+++ github-followers-app/Screens/FollowerListVC.swift
@@ -40,7 +40,7 @@ class FollowerListVC: UIViewController {
     
     
     private func configureCollectionView() {
-        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: UICollectionViewFlowLayout())
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createThreeColumnFlowLayout())
         view.addSubview(collectionView)
         collectionView.backgroundColor = .systemBackground
         collectionView.register(FollowerCell.self, forCellWithReuseIdentifier: FollowerCell.reuseID)
