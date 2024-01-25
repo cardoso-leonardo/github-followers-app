725090db9ad9f73c051cd7cf2f0c389f91b79450
Leonardo Cardoso <cardoso.leonardo@outlook.com>
2024-01-24 21:37:24 -0300

    feat: implemented the function to show empty state view

--- github-followers-app/Screens/FollowerListVC.swift
+++ github-followers-app/Screens/FollowerListVC.swift
@@ -54,11 +54,22 @@ class FollowerListVC: UIViewController {
         NetworkManager.shared.fetchFollowers(username: username, page: page) { [weak self] result in
             guard let self = self else { return }
             self.dismissLoadingView()
+            
             switch result {
             case .success(let followers):
                 if followers.count < 100 { hasMoreFollowers = false }
                 self.followers.append(contentsOf: followers)
+                
+                if self.followers.isEmpty {
+                    let message = "This user doesn't have any followers. Go follow them 😀."
+                    DispatchQueue.main.async {
+                        self.showEmptyStateView(with: message, in: self.view)
+                    }
+                    return
+                }
+                
                 self.updateData()
+                
             case .failure(let error):
                 self.presentAlertOnMainThread(title: "Ooops", message: error.rawValue, buttonTitle: "Ok")
             }
