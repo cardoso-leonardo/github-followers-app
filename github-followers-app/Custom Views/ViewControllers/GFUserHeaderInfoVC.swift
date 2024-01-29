0da5b4a8cc605150016fe249f013638d515f86ea
Leonardo Cardoso <cardoso.leonardo@outlook.com>
2024-01-29 19:58:37 -0300

    feat: inital configuration of variables and init

--- github-followers-app/Custom Views/ViewControllers/GFUserHeaderInfoVC.swift
+++ github-followers-app/Custom Views/ViewControllers/GFUserHeaderInfoVC.swift
@@ -8,22 +8,44 @@
 import UIKit
 
 class GFUserHeaderInfoVC: UIViewController {
+    
+    private var avatarImageView = GFAvatarImageView(frame: .zero)
+    private var usernameLabel = GFTitleLabel(textAlignment: .left, fontSize: 38)
+    private var secondaryLabel = GFMediumLabel(fontSize: 28)
+    private var locationImage = UIImageView()
+    private var locationLabel = GFMediumLabel(fontSize: 28)
+    private var bioLabel = GFBodyLabel(textAlignment: .left)
+    
+    private var user: User!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    init(user: User) {
+        super.init(nibName: nil, bundle: nil)
+        self.user = user
     }
-    */
+    
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 
+    
+    private func configureUI() {
+        
+    }
+    
+    
+    private func layoutUI() {
+        view.addSubviews(views:avatarImageView, usernameLabel, secondaryLabel, locationImage, locationLabel, bioLabel)
+        locationImage.translatesAutoresizingMaskIntoConstraints = false
+        
+        
+    }
+    
+    
 }
