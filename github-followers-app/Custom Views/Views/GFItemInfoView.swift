18d35aa26d1c37e94f5dd09cef4c065194770ff5
Leonardo Cardoso <cardoso.leonardo@outlook.com>
2024-01-30 17:41:01 -0300

    feat: created GFItemInfoView and Constants files

---
+++ github-followers-app/Custom Views/Views/GFItemInfoView.swift
@@ -0,0 +1,20 @@
+//
+//  GFItemInfoView.swift
+//  github-followers-app
+//
+//  Created by Leonardo Cardoso on 30/01/24.
+//
+
+import UIKit
+
+class GFItemInfoView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ github-followers-app/Utilities/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  github-followers-app
+//
+//  Created by Leonardo Cardoso on 30/01/24.
+//
+
+import Foundation
+
+enum SFSymbols {
+    static let location = "mappin.and.ellipse"
+    static let repos = "folder"
+    static let gists = "text.alignleft"
+    static let followers = "heart"
+    static let following = "person.2"
+}
