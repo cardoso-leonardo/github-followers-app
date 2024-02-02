08e32917979574ceec92d455e0d6800e33ae307b
Leonardo Cardoso <cardoso.leonardo@outlook.com>
2024-02-02 18:47:51 -0300

    feat: created new error messages

---
+++ github-followers-app/Managers/PersistenceManager.swift
@@ -0,0 +1,8 @@
+//
+//  PersistenceManager.swift
+//  github-followers-app
+//
+//  Created by Leonardo Cardoso on 02/02/24.
+//
+
+import Foundation
--- github-followers-app/Utilities/GFError.swift
+++ github-followers-app/Utilities/GFError.swift
@@ -8,9 +8,16 @@
 import Foundation
 
 enum GFError: String, Error {
+    
+    //MARK: NetworkManager
     case invalidUsername = "This username created an invalid request. Please try again."
     case unableToComplete = "Unable to complete your request. Please check your internet connection."
     case invalidResponse = "Invalid response from the server. Please try again."
     case invalidData = "The data received from the server was invalid. Please try again."
+    
+    //MARK: PersistenceManager
+    case unableToRetrieveFavorites = "Unable to retrieve favorites. Please try again later"
+    case unableToSaveFavorites = "Unable to save favorites"
+    case alreadyMarkedAsFavorite = "This user has already been marked as favorites"
 }
 
