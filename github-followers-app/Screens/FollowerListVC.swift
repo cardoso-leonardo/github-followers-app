e76c6720c050f7d6052cdcffb541d732d78a1255
Leonardo Cardoso <cardoso.leonardo@outlook.com>
2024-02-02 17:38:08 -0300

    feat: created addButton

--- github-followers-app/Screens/FollowerListVC.swift
+++ github-followers-app/Screens/FollowerListVC.swift
@@ -45,6 +45,8 @@ class FollowerListVC: UIViewController {
     private func configureViewController() {
         view.backgroundColor = .systemBackground
         navigationController?.navigationBar.prefersLargeTitles = true
+        let addButton = UIBarButtonItem(image: UIImage(systemName: "heart"), style: .plain, target: self, action: #selector(addButtonTapped))
+        navigationItem.rightBarButtonItem = addButton
     }
     
     
@@ -109,6 +111,11 @@ class FollowerListVC: UIViewController {
         snapshot.appendSections([.main])
         snapshot.appendItems(followers)
         DispatchQueue.main.async { self.dataSource.apply(snapshot, animatingDifferences: true) }
+    }
+    
+    
+    @objc func addButtonTapped() {
+        
     }
     
 }
