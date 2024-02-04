6a9bcb147457b58ef0d9303aa775c6493442c668
Leonardo Cardoso <cardoso.leonardo@outlook.com>
2024-02-03 23:28:58 -0300

    feat: created FavoriteCell

---
+++ github-followers-app/Custom Views/Cells/FavoriteCell.swift
@@ -0,0 +1,23 @@
+//
+//  FavoriteCell.swift
+//  github-followers-app
+//
+//  Created by Leonardo Cardoso on 03/02/24.
+//
+
+import UIKit
+
+class FavoriteCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
