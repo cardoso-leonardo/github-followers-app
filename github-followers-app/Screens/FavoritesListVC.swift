542b0a5b87492f75819a770c2a2ad75323fd8ed6
Leonardo Cardoso <cardoso.leonardo@outlook.com>
2024-02-05 18:15:41 -0300

    feat: inital config FavoritesListVC

--- github-followers-app/Screens/FavoritesListVC.swift
+++ github-followers-app/Screens/FavoritesListVC.swift
@@ -8,11 +8,30 @@
 import UIKit
 
 class FavoritesListVC: UIViewController {
+    
+    private var tableView: UITableView!
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBlue
         
+        
+    }
+    
+    
+    
+    private func configureTableView() {
+        
+    }
+    
+    
+    private func configure() {
+        
+    }
+    
+    
+    private func fetchFavorites() {
         PersistenceManager.retrieveFavorites { result in
             switch result {
             case .success(let favorites):
@@ -22,4 +41,5 @@ class FavoritesListVC: UIViewController {
             }
         }
     }
+
 }
