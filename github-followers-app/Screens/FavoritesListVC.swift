f106767d1a65f5cc8906d5cdf5dd411600b0280d
Leonardo Cardoso <cardoso.leonardo@outlook.com>
2024-02-14 19:26:08 -0300

    fix: the swipe to delete could potentially remove data from UI but not from storage

--- github-followers-app/Screens/FavoritesListVC.swift
+++ github-followers-app/Screens/FavoritesListVC.swift
@@ -32,6 +32,7 @@ final class FavoritesListVC: GFDataLoadingVC {
         tableView.delegate = self
         tableView.dataSource = self
         tableView.rowHeight = 80
+        tableView.removeExcessCells()
         
         tableView.register(FavoriteCell.self, forCellReuseIdentifier: FavoriteCell.reuseID)
     }
@@ -90,13 +91,14 @@ extension FavoritesListVC: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         guard editingStyle == .delete else { return }
-        let favorite = favorites[indexPath.row]
-        favorites.remove(at: indexPath.row)
-        tableView.deleteRows(at: [indexPath], with: .left)
         
-        PersistenceManager.updateWith(favorite: favorite, actionType: .remove) { [weak self] error in
+        PersistenceManager.updateWith(favorite: favorites[indexPath.row], actionType: .remove) { [weak self] error in
             guard let self = self else { return }
-            guard let error = error else { return }
+            guard let error = error else {
+                favorites.remove(at: indexPath.row)
+                tableView.deleteRows(at: [indexPath], with: .left)
+                return
+            }
             self.presentAlertOnMainThread(title: "Something went wrong", message: error.rawValue, buttonTitle: "Ok")
         }
         
